18fa99cf773423433d61dd74551a199f2f6798d8
Ovi sanjaya <ovi.sanjaya11@gmail.com>
2022-04-21 07:02:06 +0700

    Update db.go

--- config/db.go
+++ config/db.go
@@ -2,7 +2,6 @@ package config
 
 import (
 	"fmt"
-	"os"
 
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
@@ -14,11 +13,11 @@ var err error
 func InitDB() {
 	LoadEnv()
 
-	username := os.Getenv("DB_USERNAME")
-	password := os.Getenv("DB_PASSWORD")
-	host := os.Getenv("DB_HOST")
-	port := os.Getenv("DB_PORT")
-	databaseName := os.Getenv("DB_NAME")
+	username := "root"
+	password := ""
+	host := "localhost"
+	port := "3306"
+	databaseName := "todo"
 
 	dsn := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8mb4&parseTime=True&loc=Local", username, password, host, port, databaseName)
 	DB, err = gorm.Open(mysql.Open(dsn), &gorm.Config{})
